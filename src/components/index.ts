9e9c14a09903c6d797d8226f2f1557bd4b53fb9c
explore3363-coder <274944241+explore3363-coder@users.noreply.github.com>
2026-05-08 14:36:44 +0800

    chore: remove 3 unused component files (OverviewCard, GlassCard, Icons)

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 // Component exports
 export {MetricCard}     from './MetricCard';
 export {SectionTitle}   from './SectionTitle';
-export {OverviewCard}   from './OverviewCard';
 export {StoreCard}      from './StoreCard';
 export {FeedItem}       from './FeedItem';
 export {TaskBadge}      from './TaskBadge';
